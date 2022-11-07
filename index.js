661727d388db536f986c34ddc3d303339ec149d2
Courtney Hampton <jaye0888@gmail.com>
2022-11-07 11:19:57 +0100

    Adding tr to table
    adding input and clearing modal on add

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const deleteItem = (event) => {
 for (let i = 0; i < deletebtns.length; i++) {
   deletebtns[i].addEventListener("click", deleteItem);
 }
+//delete btns display one at a time ? maybe later
+const tableRow = document.querySelectorAll("tr");
+
+const displayBtn = () => {};
+
+for (let i = 0; i < tableRow.length; i++) {
+  tableRow[i].addEventListener("mouseover", displayBtn);
+}
 //Modal stuff
 let modal = document.getElementById("modalBox");
 let addbtn = document.getElementById("addtrack");
@@ -32,8 +40,47 @@ const exitModal = () => {
 xBtn.addEventListener("click", exitModal);
 modalNo.addEventListener("click", exitModal);
 
+//add to list based on input
+const trackNumInput = document.getElementById("trackNum");
+const trackNameInput = document.getElementById("trackTitle");
+const trackLengthInput = document.getElementById("Duration");
+
+const clearModal = () => {
+  trackNameInput.value = null;
+  trackNumInput.value = null;
+  trackLengthInput.value = null;
+};
+
 const addTrack = () => {
-  console.log("pushed");
+  let trackNum = trackNumInput.value;
+  let trackName = trackNameInput.value;
+  let duration = trackLengthInput.value;
+
+  const tbody = document.getElementById("table1");
+  const tRow = document.createElement("tr");
+  const thead = document.createElement("th");
+  const tdata = document.createElement("td");
+  const tdata2 = document.createElement("td");
+  const tdata3 = document.createElement("td");
+  const dbtn = document.createElement("button");
+
+  tbody.appendChild(tRow);
+  thead.setAttribute("scope", "row");
+  thead.innerText = trackNum;
+  tRow.appendChild(thead);
+  tdata.innerText = trackName;
+  tRow.appendChild(tdata);
+  tdata2.setAttribute("class", "time");
+  tdata2.innerText = duration;
+  tRow.appendChild(tdata2);
+  dbtn.innerText = "Delete";
+  dbtn.classList.add("btn", "btn-danger", "hide");
+  tdata3.appendChild(dbtn);
+  tRow.appendChild(tdata3);
+
+  console.log(trackNum, trackName, duration);
+  clearModal();
+  exitModal();
 };
 
 modalYes.addEventListener("click", addTrack);
